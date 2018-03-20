772b9e37da70adec37264f020f9dc99c3dcf3fd6
Bovojon <bovojon@gmail.com>
2018-03-20 13:36:36 -0400

    Add plurality_value()

--- readFile.py
+++ readFile.py
@@ -1,18 +1,32 @@
 def readFile():
-    value_dict = {}
+    attr_dict = {}
     lines_list = []
     f = open("pets.txt", 'r')
     firstLineStr = f.readline()
     firstline = firstLineStr.split()
     for attribute in firstline:
-        value_dict[attribute] = []
+        attr_dict[attribute] = []
     with open("pets.txt", 'r') as f:
         next(f)
         for line in f:
             fields = line.split('\t')
             for i in range(len(firstline)):
-                value_dict[firstline[i]].append(fields[i])
-    print(value_dict)
+                attr_dict[firstline[i]].append(fields[i])
+    return attr_dict
+
+
+def plurality_value(attribute, attr_dict):
+    value_dict = {}
+    values_list = attr_dict[attribute]
+    for value in values_list:
+        if value in value_dict:
+            value_dict[value] += 1
+        else:
+            value_dict[value] = 1
+    plurality = max(value_dict,key=value_dict.get)
+    print(plurality)
+
 
 if __name__ == '__main__':
-    readFile()
+    attr_dict = readFile()
+    plurality_value("size", attr_dict)
